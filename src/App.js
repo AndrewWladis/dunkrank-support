d818c79a8b861f142a682c01702287346a5045aa
Andrew Wladis <andrewwladis@gmail.com>
2023-05-15 21:36:52 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Item from './Item';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className='header'>Dunk Rank Support</h1>
       </header>
+      <Item data={{
+        title: "Username Rules",
+        info: "Usernames must be all lowercase, not taken, not have the @ symbol, and have no spaces."
+      }}/>
+      <Item data={{
+        title: "Password Rules",
+        info: "Passwords must be at least 3 characters."
+      }}/>
+      <Item data={{
+        title: "Email Rules",
+        info: "Emails must be a valid."
+      }}/>
+      <Item data={{
+        title: "Submitting a Request",
+        info: "To submit a request, click the plus icon in the top right corner and fill out the details. Use a valid username. Use scores below 100."
+      }}/>
+      <Item data={{
+        title: "Approving a Request",
+        info: "To approve a request hit the check icon to the left of the score on an incoming request item. It will adjust your Rank Score accordingly."
+      }}/>
+      <Item data={{
+        title: "Denying a Request",
+        info: "To deny a request hit the x icon to the right of the score on an incoming request item. It will adjust your Rank Score accordingly."
+      }}/>
+      <Item data={{
+        title: "School/Team",
+        info: "To join a school or team select the school you are in. The cumulative team Rank Score is collected through average scores. Joining a team slightly increases the teams Rank Score."
+      }}/>
+      <Item data={{
+        title: "Profile Pictures",
+        info: "To change your profile picture, select an icon on the profile page. Certain profile pictures can only be accessed if you have your Rank Score is above a certain number."
+      }}/>
+      <Item data={{
+        title: "Deleting an Account",
+        info: "To delete an account, hit the delete account button above the icons. It will then ask you to confirm your decision."
+      }}/>
+      <h1 className='header-color'>Support Contact</h1>
+      <Item data={{
+        title: "Contact Email",
+        info: "andywladis19@gmail.com"
+      }}/>
     </div>
   );
 }
---
+++ src/Item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+function Item({data}) {
+  return (
+    <div className="Item">
+        <h2>{data.title}</h2>
+        <h3 className='info'>- {data.info}</h3>
+    </div>
+  )
+}
+
+export default Item
